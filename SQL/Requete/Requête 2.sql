2550df3da48f72164aabe8f19764134ea2fcc9da
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-06-25 14:55:59 +0200

    fix(Requete 2)

--- SQL/Requete/Requête 2.sql
+++ SQL/Requete/Requête 2.sql
@@ -1,5 +1,5 @@
    SELECT movie.name,date_release ,floor(duration/60) AS heure, (duration%60) AS minute, person.name , person.firstname
 	FROM movie,director,person
-	WHERE movie.id_director = director.id_director AND director.id_person = person.id_person AND duration > 135;
-
+	WHERE movie.id_director = director.id_director AND director.id_person = person.id_person AND duration > 135
+	ORDER BY duration DESC
 
