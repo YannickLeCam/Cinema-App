617d2c883faf1ed527ff3f761d13ee7f4ae1ca5b
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-06-25 16:46:03 +0200

    add(requete 3 4)

--- SQL/Requete/Requête 4.sql
+++ SQL/Requete/Requête 4.sql
@@ -1,5 +1,6 @@
-SELECT NAME , COUNT(id_movie) 
+SELECT NAME , COUNT(id_movie) AS "Nb Movie"
 FROM type
 JOIN be
 ON be.id_type = type.id_type
-GROUP BY be.id_type;
+GROUP BY be.id_type
+ORDER BY "Nb movie" DESC;
