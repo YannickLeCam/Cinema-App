e5090f6879383a26d01e380999208792d7c8c6be
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-06-26 09:45:31 +0200

    fix(requete 1 2)

--- SQL/Requete/Requête 1.sql
+++ SQL/Requete/Requête 1.sql
@@ -1,4 +1,4 @@
-   SELECT movie.name,date_release ,floor(duration/60) AS "heure", (duration%60) AS "minute", person.name , person.firstname
+   SELECT movie.name,date_release ,CONCAT(floor(duration/60),' h ' , (duration%60),' min') AS durée, person.name , person.firstname
 	FROM movie,director,person
 	WHERE movie.id_director = director.id_director AND director.id_person = person.id_person;
 
