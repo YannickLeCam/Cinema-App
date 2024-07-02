32d9d5a4d9b3accbf5036da2f1439b5eea4a35ee
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-07-02 10:39:12 +0200

    add(fctn create cards role)

--- view/listRoles.php
+++ view/listRoles.php
@@ -1,6 +1,13 @@
 <?php ob_start();
 
 
+function createCardsTypes($listRoles){
+    $htmlContent ="";
+    foreach ($listRoles as $role) {
+        $htmlContent .= '<a href="./index.php?action=detailRole&id='.$role['id_role'].'"><div class="card"><h4>'.$role['name'].'</h4></div></a>';
+    }
+    return $htmlContent;
+}
 
 use Service\NavService;
 
@@ -9,15 +16,15 @@ echo $navService->navList();
 ?>
 
 
-
-<pre>
-    <?=var_dump($listRoles)?>
-</pre>
-
-
-
-
-
+<div id="listCardWithoutPicture">
+    <div id="research">
+        <form action="./index.php?action=listActors" method="post">
+            <input type="text" name="titleContain" id="">
+            <input type="submit" name="SubmitSearchButton" value="Rechercher">
+        </form>
+    </div>
+    <?=createCardsTypes($listRoles)?>
+</div>
 
 
 <?php
