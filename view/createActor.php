df6bf1dfb5c4c65312d875136c34ce5fdac74d16
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-07-02 14:20:13 +0200

    add(Boostrap on Actor / Director Form)

--- view/createActor.php
+++ view/createActor.php
@@ -12,19 +12,30 @@ echo $navService->navCreate();
 ?>
 
 <form action="./index.php?action=createActor" method="post">
-    <label for="name">Entrer le prénom :</label>
-    <input type="text" name="firstname" id="" value="<?=isset($data["firstname"])?htmlentities($data["firstname"]):""?>">
-    <label for="name">Entrer le nom :</label>
-    <input type="text" name="name" id="" value="<?=isset($data["name"])?htmlentities($data["name"]):""?>">
-    <label for="birthday">Date d'anniversaire:</label>
-    <input type="date" id="birthday" name="birthday" value="<?=(isset($data['birthday']) ? $data['birthday'] :"")?>">
-    <label for="genre">Sexe :</label>
-    <select name="genre" id="">
-        <option value="Male" <?=(isset($data['genre'])? ($data['genre']=='Male'?"selected":"" ):"" )?>>Male</option>
-        <option value="Female" <?=(isset($data['genre'])? ($data['genre']=='Female'?"selected":"" ):"" )?>>Female</option>
-        <option value="Other" <?=(isset($data['genre'])? ($data['genre']=='Other'?"selected":"" ):"" )?>>Other</option>
-    </select>
-    <input type="submit" name="SubmitActorForm" value="Ajouter">
+
+    <div class="form-floating mb-3">
+        <input type="text" name="firstname" id="floatingInput" class="form-control" value="<?=isset($data["firstname"])?htmlentities($data["firstname"]):""?>" placeholder="Entrer le prénom :">
+        <label for="floatingInput">Entrer le prénom :</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="text" name="name" id="floatingInput" class="form-control" value="<?=isset($data["name"])?htmlentities($data["name"]):""?>" placeholder="EEntrer le nom :">
+        <label for="floatingInput">Entrer le nom :</label>
+    </div>
+
+
+    <div class="form-sub-container">
+
+        <label for="birthday">Date d'anniversaire:</label>
+        <label for="genre">Genre :</label>
+        <input type="date" id="birthday" name="birthday" class="form-control" value="<?=(isset($data['birthday']) ? $data['birthday'] :"")?>">
+        <select name="genre" id="" class="form-select-lg mb-3">
+            <option value="Male" <?=(isset($data['genre'])? ($data['genre']=='Male'?"selected":"" ):"" )?>>Male</option>
+            <option value="Female" <?=(isset($data['genre'])? ($data['genre']=='Female'?"selected":"" ):"" )?>>Female</option>
+            <option value="Other" <?=(isset($data['genre'])? ($data['genre']=='Other'?"selected":"" ):"" )?>>Other</option>
+        </select>
+    </div>
+
+    <input type="submit" name="SubmitActorForm" value="Ajouter" class="btn btn-secondary">
 </form>
 
 
