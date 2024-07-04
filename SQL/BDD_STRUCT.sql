00d4c79937021b2ef4c87ee43f734a80406e521f
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-07-04 16:56:07 +0200

    Update BDD_STRUCT.sql

--- SQL/BDD_STRUCT.sql
+++ SQL/BDD_STRUCT.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `actor` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table cinema-lecam.actor : ~5 rows (environ)
-INSERT IGNORE INTO `actor` (`id_actor`, `id_person`) VALUES
+INSERT INTO `actor` (`id_actor`, `id_person`) VALUES
 	(5, 1),
 	(1, 2),
 	(2, 4),
@@ -47,8 +47,8 @@ CREATE TABLE IF NOT EXISTS `be` (
   CONSTRAINT `FK_be_type` FOREIGN KEY (`id_type`) REFERENCES `type` (`id_type`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema-lecam.be : ~0 rows (environ)
-INSERT IGNORE INTO `be` (`id_movie`, `id_type`) VALUES
+-- Listage des données de la table cinema-lecam.be : ~6 rows (environ)
+INSERT INTO `be` (`id_movie`, `id_type`) VALUES
 	(1, 1),
 	(2, 2),
 	(3, 3),
@@ -70,12 +70,13 @@ CREATE TABLE IF NOT EXISTS `casting` (
   CONSTRAINT `FK_casting_role` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema-lecam.casting : ~0 rows (environ)
-INSERT IGNORE INTO `casting` (`id_actor`, `id_movie`, `id_role`) VALUES
+-- Listage des données de la table cinema-lecam.casting : ~13 rows (environ)
+INSERT INTO `casting` (`id_actor`, `id_movie`, `id_role`) VALUES
 	(1, 1, 2),
 	(2, 1, 1),
 	(1, 2, 1),
 	(2, 2, 3),
+	(5, 2, 4),
 	(1, 3, 4),
 	(2, 3, 5),
 	(3, 4, 6),
@@ -95,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `director` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table cinema-lecam.director : ~4 rows (environ)
-INSERT IGNORE INTO `director` (`id_director`, `id_person`) VALUES
+INSERT INTO `director` (`id_director`, `id_person`) VALUES
 	(1, 1),
 	(2, 3),
 	(3, 5),
@@ -116,14 +117,14 @@ CREATE TABLE IF NOT EXISTS `movie` (
   CONSTRAINT `FK_movie_director` FOREIGN KEY (`id_director`) REFERENCES `director` (`id_director`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema-lecam.movie : ~0 rows (environ)
-INSERT IGNORE INTO `movie` (`id_movie`, `name`, `date_release`, `duration`, `synopsis`, `poster`, `rate`, `id_director`) VALUES
-	(1, 'Epic Adventure', '2023-06-01', 120, 'An epic adventure of discovery and courage.', 'poster1.jpg', 8.5, 1),
-	(2, 'Romantic Escape', '2023-02-14', 95, 'A love story that transcends time.', 'poster2.jpg', 7.8, 2),
-	(3, 'Mystery Thriller', '2023-09-10', 150, 'A thrilling mystery that keeps you on the edge of your seat.', 'poster3.jpg', 8.2, 3),
-	(4, 'Sci-Fi Odyssey', '2023-12-20', 200, 'A journey through space and time.', 'poster4.jpg', 9.0, 4),
-	(5, 'Comedy Night', '2023-07-18', 85, 'A night full of laughs and surprises.', 'poster5.jpg', 7.5, 3),
-	(6, 'Historical Drama', '2023-11-05', 180, 'A poignant drama set in historical times.', 'poster6.jpg', 8.8, 4);
+-- Listage des données de la table cinema-lecam.movie : ~6 rows (environ)
+INSERT INTO `movie` (`id_movie`, `name`, `date_release`, `duration`, `synopsis`, `poster`, `rate`, `id_director`) VALUES
+	(1, 'Epic Adventure', '2023-06-01', 120, 'An epic adventure of discovery and courage.', 'https://folkr.fr/wp-content/uploads/2015/12/top-20-affiches-films-2015-folkr-Realite.jpg', 8.5, 1),
+	(2, 'Romantic Escape', '2023-02-14', 95, 'A love story that transcends time.', 'https://www.mauvais-genres.com/img/p/7/4/2/2/7422.jpg', 7.8, 2),
+	(3, 'Mystery Thriller', '2023-09-10', 150, 'A thrilling mystery that keeps you on the edge of your seat.', 'https://fr.web.img4.acsta.net/pictures/210/240/21024019_20130801183449394.jpg', 8.2, 3),
+	(4, 'Sci-Fi Odyssey', '2023-12-20', 200, 'A journey through space and time.', 'https://media.senscritique.com/media/000012288077/source_big/Pulp_Fiction.jpg', 9.0, 4),
+	(5, 'Comedy Night', '2023-07-18', 85, 'A night full of laughs and surprises.', 'https://th.bing.com/th/id/R.dd8b2981c9452b9ece9eafa660b42df4?rik=6nS4P%2b9KRKD1JQ&pid=ImgRaw&r=0', 7.5, 3),
+	(6, 'Historical Drama', '2023-11-05', 180, 'A poignant drama set in historical times.', 'https://fr.web.img3.acsta.net/pictures/14/09/12/17/19/272469.jpg', 8.8, 4);
 
 -- Listage de la structure de table cinema-lecam. person
 CREATE TABLE IF NOT EXISTS `person` (
@@ -133,10 +134,10 @@ CREATE TABLE IF NOT EXISTS `person` (
   `birthday` date NOT NULL,
   `genre` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id_person`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table cinema-lecam.person : ~8 rows (environ)
-INSERT IGNORE INTO `person` (`id_person`, `name`, `firstname`, `birthday`, `genre`) VALUES
+INSERT INTO `person` (`id_person`, `name`, `firstname`, `birthday`, `genre`) VALUES
 	(1, 'Smith', 'John', '1980-05-15', 'Male'),
 	(2, 'Doe', 'Jane', '1990-08-25', 'Female'),
 	(3, 'Brown', 'Charlie', '1975-12-01', 'Male'),
@@ -155,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table cinema-lecam.role : ~0 rows (environ)
-INSERT IGNORE INTO `role` (`id_role`, `name`) VALUES
+INSERT INTO `role` (`id_role`, `name`) VALUES
 	(6, 'Captain Kirk'),
 	(5, 'Dr. Watson'),
 	(2, 'Elizabeth Swann'),
@@ -172,10 +173,10 @@ CREATE TABLE IF NOT EXISTS `type` (
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id_type`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema-lecam.type : ~0 rows (environ)
-INSERT IGNORE INTO `type` (`id_type`, `name`) VALUES
+-- Listage des données de la table cinema-lecam.type : ~6 rows (environ)
+INSERT INTO `type` (`id_type`, `name`) VALUES
 	(1, 'Action'),
 	(5, 'Comedy'),
 	(6, 'Drama'),
